e923e07d77479bf77b9e87968c7fba00247e4487
sharno <sharno@qq.com>
2020-09-21 18:48:18 +0800

    init

--- index.js
+++ index.js
@@ -1,28 +1,86 @@
-module.exports = (opts = { }) => {
+module.exports = (options = {}) => {
+  const opts = Object.assign({}, {
+    //除数,转换后的值 等于 pixel / divisor
+    divisor: 1,
 
-  // Work with options here
+    //倍数，转换后的值 等于 pixel * multiple
+    multiple: 1,
+
+    // 小数点后保留的位数，例如, width: 100px中的100，
+    // 将会被转换成 Number(100 / divisor * multiple).toFixed(decimalPlaces)
+    decimalPlaces: 2,
+
+    //换单位，默认值为 rpx，如果设置其值为 'rem'，px将会被转换为rem。
+    targetUnits: 'rpx',
+
+    // 选择器, 支持正则, 比如.my- 只会替换.my-box
+    selector: null,
+
+    //只转换的属性,不在列表中的不转换,支持字符串,数组,正则
+    onlyAttr: [],
+
+    // 全部启用注释,
+    enableAllComment: 'px2units-enable',
+
+    // 全部禁用注释
+    disableAllComment: 'px2units-disable',
+
+    //不转换px单位的注释，默认为 /*no*/
+    //如果设置 comment 的值为 'not replace', width: 100px 中的100px将不会被转换为 rpx。
+    comment: 'no'
+  }, options)
+
+  /**
+   * 替换PX
+   * @param str 对象属性
+   * @returns {string|*}
+   */
+  const replacePx = (str) => {
+    if (!str) return ''
+    return str.replace(/\b(\d+(\.\d+)?)px\b/ig, function (match, x) {
+      const size = x * opts.multiple / opts.divisor
+      return size % 1 === 0 ? size + opts.targetUnits : size.toFixed(opts.decimalPlaces) + opts.targetUnits
+    })
+  }
+
+  /**
+   * 遍历规则替换CSS规则
+   * @param decl 规则对象  font-size: 16px;* no *
+   */
+  const replaceDecl = (decl) => {
+    if (decl && decl.next() && decl.next().type === 'comment' && decl.next().text === opts.comment) {
+      decl.next().remove()
+    } else {
+      decl.value = replacePx(decl.value)
+    }
+  }
 
   return {
     postcssPlugin: 'postcss-px2units-plus',
-    /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
-    */
+    Root(root) {
+      // 属性选择器
+      const declsSelector = opts.onlyAttr instanceof Array ? new RegExp(opts.onlyAttr.join('|')) : opts.onlyAttr
+      // 遍历规则
+      const walkRules = (rule) => {
+        if (rule && rule.prev() && rule.prev().type === 'comment') {   // 存在规则级注释
+          if (rule.prev().text === opts.enableAllComment) {         //全部启用注释, 则取消属性选择器
+            rule.walkDecls(replaceDecl)
+            rule.prev().remove()
+          } else if (rule.prev().text === opts.disableAllComment) { // 禁用转换,则取消操作
+            rule.prev().remove()
+          }
+        } else {
+          // 没有注释,默认操作
+          rule.walkDecls(declsSelector, replaceDecl)
+        }
+      }
 
-    /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
-
-    /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
+      if (opts.selector) {
+        root.walkRules(opts.selector, walkRules)
+      } else {
+        root.walkRules(walkRules)
       }
     }
-    */
   }
 }
 module.exports.postcss = true
--- index.test.js
+++ index.test.js
@@ -2,16 +2,190 @@ const postcss = require('postcss')
 
 const plugin = require('./')
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+async function run(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, {from: undefined})
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
-
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+it('replace pixel values', async () => {
+  await run(`.title{
+      font-size: 24px;
+      margin: 0 0 0 5px;
+      vertical-align: -1px;
+      display: flex;
+    }`, `.title{
+      font-size: 24rpx;
+      margin: 0 0 0 5rpx;
+      vertical-align: -1rpx;
+      display: flex;
+    }`, {})
 })
 
-*/
+it('rpx not be replaced', async () => {
+  await run(`.title{ padding: 20rpx 30px 2rem 4em;}`,
+    `.title{ padding: 20rpx 30rpx 2rem 4em;}`, {})
+})
+
+it('pixel values not be replaced', async () => {
+  await run(`.title{
+      padding: 20rpx 30px 2rem 4em; /* no */
+      margin: 40px;
+      font-size: 24px; /* no */
+    }`, `.title{
+      padding: 20rpx 30px 2rem 4em;
+      margin: 40rpx;
+      font-size: 24px;
+    }`, {})
+})
+
+it('replace pixel values with px * opts.multiple', async () => {
+  await run(`.title{
+      padding: 30px;
+      margin: 40px;
+    }`, `.title{
+      padding: 60rpx;
+      margin: 80rpx;
+    }`, {multiple: 2})
+})
+
+it('replace pixel values with rem units', async () => {
+  await run(`.title{
+     padding: 30px;
+     margin: 40px;
+    }`, `.title{
+     padding: 10rem;
+     margin: 13.333rem;
+    }`, {
+    divisor: 3,
+    decimalPlaces: 3,
+    targetUnits: 'rem'
+  })
+})
+
+
+it('replace pixel in selector', async () => {
+  await run(`.title{
+     padding: 30px;
+     margin: 40px;
+    }
+    .title1{
+     padding: 30px;
+     margin: 40px;
+    }`, `.title{
+     padding: 30px;
+     margin: 40px;
+    }
+    .title1{
+     padding: 30rpx;
+     margin: 40rpx;
+    }`, {
+    selector: '.title1'
+  })
+})
+
+
+it('replace pixel in regexp selector', async () => {
+  await run(`.title{
+     padding: 30px;
+     margin: 40px;
+    }
+    .my-title-a{
+     padding: 30px;
+     margin: 40px;
+    }`, `.title{
+     padding: 30rpx;
+     margin: 40rpx;
+    }
+    .my-title-a{
+     padding: 30px;
+     margin: 40px;
+    }`, {
+    selector: /.title$/
+  })
+})
+
+it('disable replace comment', async () => {
+  await run(`.title{
+     padding: 30px;
+     margin: 40px;
+    }
+    /* px2units-disable */
+    .my-title{
+     padding: 30px;
+     margin: 40px;
+    }`, `.title{
+     padding: 30rpx;
+     margin: 40rpx;
+    }
+    .my-title{
+     padding: 30px;
+     margin: 40px;
+    }`, {
+    selector: /.title$/
+  })
+})
+
+it('Force all properties to be replaced', async () => {
+  await run(`.title{
+     padding: 30px;
+     margin: 40px;
+    }
+    /*px2units-enable*/
+    .my-title-a{
+     padding: 30px;
+     margin: 40px;
+    }`, `.title{
+     padding: 30rpx;
+     margin: 40px;
+    }
+    .my-title-a{
+     padding: 30rpx;
+     margin: 40rpx;
+    }`, {
+    onlyAttr: ['padding']
+  })
+})
+
+it('Replace only the specified attribute', async () => {
+  await run(`.title{
+     padding: 30px;
+     margin: 40px;
+    }
+    .my-title-a{
+     padding: 30px;
+     margin: 40px;
+    }`, `.title{
+     padding: 30rpx;
+     margin: 40px;
+    }
+    .my-title-a{
+     padding: 30rpx;
+     margin: 40px;
+    }`, {
+    onlyAttr: ['padding']
+  })
+})
+
+
+it('work in media', async () => {
+  await run(`@media (-webkit-min-device-pixel-ratio: 2), (min-device-pixel-ratio: 2) {
+   .word {
+        margin-top: 30px;
+        margin-bottom: 40px;
+      }
+      .word-retina {
+        margin-top: 50px;
+        margin-bottom: 60px;
+      }
+    }`, `@media (-webkit-min-device-pixel-ratio: 2), (min-device-pixel-ratio: 2) {
+   .word {
+        margin-top: 30rpx;
+        margin-bottom: 40rpx;
+      }
+      .word-retina {
+        margin-top: 50rpx;
+        margin-bottom: 60rpx;
+      }
+    }`, {})
+})
